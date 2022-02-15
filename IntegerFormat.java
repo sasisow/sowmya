a9f2e159ff773692cec626b8d0526c038132517c
sasisow <99412359+sasisow@users.noreply.github.com>
2022-02-15 13:13:13 +0530

    Assignment

---
+++ IntegerFormat.java
@@ -0,0 +1,15 @@
+package com.devlabs.assignment2;
+
+import java.util.*;
+
+public class IntegerFormat {
+	
+	public static void main( String[] arg)
+		{
+	int age;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please Enter your age : ");
+	age = Integer.parseInt(sc.next());
+	System.out.println("Your age is : " +age);
+	}
+	}
