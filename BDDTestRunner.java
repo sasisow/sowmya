8172f0b00aaf52466cb98ebf6628663efa32deff
sasisow <99412359+sasisow@users.noreply.github.com>
2022-02-15 17:56:12 +0530

    Cucumber Assignment

---
+++ BDDTestRunner.java
@@ -0,0 +1,15 @@
+package com.qa.BBDTestRunner;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(	
+		features= "C:\\Users\\grandhisr\\eclipse-workspace\\SeleniumAssignment\\src\\test\\java\\com\\qa\\Features\\OpenSource.feature",
+		glue="com.qa.StepDefinition",
+		dryRun = false,
+		strict = true,
+		monochrome = true,
+		format = {"pretty" , "html:test-output"}
+		)
+public class OpenSource {
+}
