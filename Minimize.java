7f482c9d012fbe883946ec522e329b2327c97b2f
sasisow <99412359+sasisow@users.noreply.github.com>
2022-02-15 17:51:48 +0530

    minimize_Selenium

---
+++ Minimize.java
@@ -0,0 +1,23 @@
+package SeleniumTestscripts;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase9{
+	
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Soft\\Jarfiles\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().setPosition(new Point(-1000, 0));
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
+	}
+
+
+}
